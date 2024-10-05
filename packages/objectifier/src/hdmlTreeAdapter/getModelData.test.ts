45c2ba8430ea692955371782d10769ed4de3238a
hdml <hdml.github@gmail.com>
2024-10-05 17:34:52 +0000

    getModelData tests

---
+++ packages/objectifier/src/hdmlTreeAdapter/getModelData.test.ts
@@ -0,0 +1,31 @@
+/**
+ * @author Artem Lytvynov
+ * @copyright Artem Lytvynov
+ * @license Apache-2.0
+ */
+
+/* eslint-disable max-len */
+
+import { IModel } from "@hdml/schemas";
+import { getModelData } from "./getModelData";
+import { MODEL_ATTRS_LIST } from "../enums/MODEL_ATTR_LIST";
+
+describe("The `getModelData` function", () => {
+  it("shoud return `null` if empty attributes passed", () => {
+    expect(getModelData([])).toBeNull();
+  });
+
+  it("shoud return `null` if incorrect attributes passed", () => {
+    expect(getModelData([{ name: "a", value: "b" }])).toBeNull();
+  });
+
+  it("shoud return `IModel` object if correct attributes passed", () => {
+    const model = getModelData([
+      { name: MODEL_ATTRS_LIST.NAME, value: "value" },
+    ]) as IModel;
+
+    expect(model).not.toBeNull();
+    expect(Object.hasOwn(model, MODEL_ATTRS_LIST.NAME)).toBeTruthy();
+    expect(model.name).toBe("value");
+  });
+});
