23b070586fb16b42ff5647a300261059a5a55d10
hdml <hdml.github@gmail.com>
2024-10-23 20:43:05 +0000

    stringlify model wip

--- packages/stringlifier/src/sql/filter.ts
+++ packages/stringlifier/src/sql/filter.ts
@@ -11,6 +11,7 @@ import {
   KeysParametersStruct,
   NamedParametersStruct,
   FilterTypeEnum,
+  FilterOperatorEnum,
 } from "@hdml/schemas";
 import {
   FilterClause,
@@ -19,6 +20,69 @@ import {
   KeysParameters,
   NamedParameters,
 } from "@hdml/types";
+import { t } from "../constants";
+
+export function getFilterClauseSQL(
+  clause: FilterClause,
+  level = 0,
+  join?: { left: string; right: string },
+): string {
+  const prefix = t.repeat(level);
+
+  let op = "";
+  let sql = "";
+
+  switch (clause.type) {
+    case FilterOperatorEnum.And:
+      sql = sql + `${prefix}1 = 1\n`;
+      op = `${prefix}and `;
+      break;
+    case FilterOperatorEnum.Or:
+      sql = sql + `${prefix}1 != 1\n`;
+      op = `${prefix}or `;
+      break;
+    case FilterOperatorEnum.None:
+    default:
+      op = `${prefix}`;
+      break;
+  }
+
+  sql =
+    sql +
+    clause.filters
+      .map((f) => `${op}${getFilterSQL(f, join)}\n`)
+      .join("");
+
+  clause.children.forEach((child) => {
+    sql = sql + `${op}(\n`;
+    sql = sql + getFilterClauseSQL(child, level + 1, join);
+    sql = sql + `${prefix})\n`;
+  });
+
+  return sql;
+}
+
+export function getFilterSQL(
+  filter: Filter,
+  join?: { left: string; right: string },
+): string {
+  switch (filter.type) {
+    case FilterTypeEnum.Keys:
+      // if (
+      //   !join?.left ||
+      //   !join?.right ||
+      //   !filter.options.left ||
+      //   !filter.options.right
+      // ) {
+      //   return "";
+      // }
+      return getKeysFilterSQL(join, filter);
+    case FilterTypeEnum.Expression:
+      return getExpressionFilterSQL(filter);
+    case FilterTypeEnum.Named:
+      return getNamedFilterSQL(filter);
+  }
+}
 
 export function objectifyJoinClause(
   clause: FilterClauseStruct,
