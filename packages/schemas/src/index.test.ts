8d6023012c7fe3224e2bac00334b02278f48f588
hdml <hdml.github@gmail.com>
2024-11-09 22:09:16 +0000

    schemas added

---
+++ packages/schemas/src/index.test.ts
@@ -0,0 +1,57 @@
+/**
+ * @author Artem Lytvynov
+ * @copyright Artem Lytvynov
+ * @license Apache-2.0
+ */
+
+import * as schema from "./index";
+
+describe("@hdml/schema", () => {
+  it("must exports required types", () => {
+    expect(Object.keys(schema).length).toBe(43);
+    expect(schema.AggregationTypeEnum).toBeDefined();
+    expect(schema.ConnectorTypesEnum).toBeDefined();
+    expect(schema.DataTypeEnum).toBeDefined();
+    expect(schema.DateUnitEnum).toBeDefined();
+    expect(schema.DecimalBitWidthEnum).toBeDefined();
+    expect(schema.FilterNameEnum).toBeDefined();
+    expect(schema.FilterOperatorEnum).toBeDefined();
+    expect(schema.FilterTypeEnum).toBeDefined();
+    expect(schema.JoinTypeEnum).toBeDefined();
+    expect(schema.OrderTypeEnum).toBeDefined();
+    expect(schema.TableTypeEnum).toBeDefined();
+    expect(schema.TimeUnitEnum).toBeDefined();
+    expect(schema.TimeZoneEnum).toBeDefined();
+
+    expect(schema.BigQueryParametersStruct).toBeDefined();
+    expect(schema.CommonParametersStruct).toBeDefined();
+    expect(schema.ConnectionStruct).toBeDefined();
+    expect(schema.ConnectionOptionsStruct).toBeDefined();
+    expect(schema.ConnectionParametersStruct).toBeDefined();
+    expect(schema.DataTypeOptionsStruct).toBeDefined();
+    expect(schema.DateParametersStruct).toBeDefined();
+    expect(schema.DecimalParametersStruct).toBeDefined();
+    expect(schema.HDOMStruct).toBeDefined();
+    expect(schema.ElasticsearchParametersStruct).toBeDefined();
+    expect(schema.ExpressionParametersStruct).toBeDefined();
+    expect(schema.FieldStruct).toBeDefined();
+    expect(schema.FieldTypeStruct).toBeDefined();
+    expect(schema.FilterStruct).toBeDefined();
+    expect(schema.FilterClauseStruct).toBeDefined();
+    expect(schema.FilterOptionsStruct).toBeDefined();
+    expect(schema.FrameStruct).toBeDefined();
+    expect(schema.GoogleSheetsParametersStruct).toBeDefined();
+    expect(schema.IncludeStruct).toBeDefined();
+    expect(schema.JDBCParametersStruct).toBeDefined();
+    expect(schema.JoinStruct).toBeDefined();
+    expect(schema.KeysParametersStruct).toBeDefined();
+    expect(schema.ModelStruct).toBeDefined();
+    expect(schema.MongoDBParametersStruct).toBeDefined();
+    expect(schema.NamedParametersStruct).toBeDefined();
+    expect(schema.SnowflakeParametersStruct).toBeDefined();
+    expect(schema.TableStruct).toBeDefined();
+    expect(schema.TimeParametersStruct).toBeDefined();
+    expect(schema.TimestampParametersStruct).toBeDefined();
+    expect(schema.UnspecifiedParametersStruct).toBeDefined();
+  });
+});
---
+++ packages/schemas/src/index.ts
@@ -0,0 +1,54 @@
+/**
+ * @author Artem Lytvynov
+ * @copyright Artem Lytvynov
+ * @license Apache-2.0
+ */
+
+/* eslint-disable max-len */
+
+// Enums
+export { AggregationTypeEnum } from "./enum/aggregation-type-enum";
+export { ConnectorTypesEnum } from "./enum/connector-types-enum";
+export { DataTypeEnum } from "./enum/data-type-enum";
+export { DateUnitEnum } from "./enum/date-unit-enum";
+export { DecimalBitWidthEnum } from "./enum/decimal-bit-width-enum";
+export { FilterNameEnum } from "./enum/filter-name-enum";
+export { FilterOperatorEnum } from "./enum/filter-operator-enum";
+export { FilterTypeEnum } from "./enum/filter-type-enum";
+export { JoinTypeEnum } from "./enum/join-type-enum";
+export { OrderTypeEnum } from "./enum/order-type-enum";
+export { TableTypeEnum } from "./enum/table-type-enum";
+export { TimeUnitEnum } from "./enum/time-unit-enum";
+export { TimeZoneEnum } from "./enum/time-zone-enum";
+
+// Structures
+export { BigQueryParametersStruct } from "./document/big-query-parameters-struct";
+export { CommonParametersStruct } from "./document/common-parameters-struct";
+export { ConnectionStruct } from "./document/connection-struct";
+export { ConnectionOptionsStruct } from "./document/connection-options-struct";
+export { ConnectionParametersStruct } from "./document/connection-parameters-struct";
+export { DataTypeOptionsStruct } from "./document/data-type-options-struct";
+export { DateParametersStruct } from "./document/date-parameters-struct";
+export { DecimalParametersStruct } from "./document/decimal-parameters-struct";
+export { HDOMStruct } from "./document/hdomstruct";
+export { ElasticsearchParametersStruct } from "./document/elasticsearch-parameters-struct";
+export { ExpressionParametersStruct } from "./document/expression-parameters-struct";
+export { FieldStruct } from "./document/field-struct";
+export { FieldTypeStruct } from "./document/field-type-struct";
+export { FilterStruct } from "./document/filter-struct";
+export { FilterClauseStruct } from "./document/filter-clause-struct";
+export { FilterOptionsStruct } from "./document/filter-options-struct";
+export { FrameStruct } from "./document/frame-struct";
+export { GoogleSheetsParametersStruct } from "./document/google-sheets-parameters-struct";
+export { IncludeStruct } from "./document/include-struct";
+export { JDBCParametersStruct } from "./document/jdbcparameters-struct";
+export { JoinStruct } from "./document/join-struct";
+export { KeysParametersStruct } from "./document/keys-parameters-struct";
+export { ModelStruct } from "./document/model-struct";
+export { MongoDBParametersStruct } from "./document/mongo-dbparameters-struct";
+export { NamedParametersStruct } from "./document/named-parameters-struct";
+export { SnowflakeParametersStruct } from "./document/snowflake-parameters-struct";
+export { TableStruct } from "./document/table-struct";
+export { TimeParametersStruct } from "./document/time-parameters-struct";
+export { TimestampParametersStruct } from "./document/timestamp-parameters-struct";
+export { UnspecifiedParametersStruct } from "./document/unspecified-parameters-struct";
