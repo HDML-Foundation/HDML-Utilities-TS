cbda9722f0dab2d4b9954f1216e8f1822c1fd9a1
hdml <hdml.github@gmail.com>
2024-11-09 18:36:21 +0000

    stringifier export fix

--- packages/stringifier/src/index.ts
+++ packages/stringifier/src/index.ts
@@ -4,6 +4,30 @@
  * @license Apache-2.0
  */
 
+import { getConnectionSQLs, getConnectionHTML } from "./connection";
+import { getModelSQL, getModelHTML } from "./model";
+import { getFrameSQL, getFrameHTML } from "./frame";
+
 export { getConnectionSQLs, getConnectionHTML } from "./connection";
 export { getModelSQL, getModelHTML } from "./model";
 export { getFrameSQL, getFrameHTML } from "./frame";
+
+const _export = globalThis as unknown as {
+  "@hdml/stringifier": {
+    getConnectionSQLs: typeof getConnectionSQLs;
+    getConnectionHTML: typeof getConnectionHTML;
+    getModelSQL: typeof getModelSQL;
+    getModelHTML: typeof getModelHTML;
+    getFrameSQL: typeof getFrameSQL;
+    getFrameHTML: typeof getFrameHTML;
+  };
+};
+
+_export["@hdml/stringifier"] = {
+  getConnectionSQLs,
+  getConnectionHTML,
+  getModelSQL,
+  getModelHTML,
+  getFrameSQL,
+  getFrameHTML,
+};
