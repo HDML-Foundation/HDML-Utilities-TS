10e32842562cceaf180608eaf310057147fe267d
hdml-github <hdml.github@gmail.com>
2026-01-25 15:40:20 +0000

    feat(hash): export base64ToBytes and bytesToBase64 functions

--- packages/hash/src/index.ts
+++ packages/hash/src/index.ts
@@ -9,12 +9,16 @@ import { uid } from "./uid";
 import { hashify } from "./hashify";
 import { hashtime } from "./hashtime";
 import { parsetime } from "./parsetime";
+import { bytesToBase64 } from "./bytesToBase64";
+import { base64ToBytes } from "./base64ToBytes";
 
 export { md5 } from "./md5";
 export { uid } from "./uid";
 export { hashify } from "./hashify";
 export { hashtime } from "./hashtime";
 export { parsetime } from "./parsetime";
+export { bytesToBase64 } from "./bytesToBase64";
+export { base64ToBytes } from "./base64ToBytes";
 
 const _export = globalThis as unknown as {
   "@hdml/hash": {
@@ -23,6 +27,8 @@ const _export = globalThis as unknown as {
     hashify: typeof hashify;
     hashtime: typeof hashtime;
     parsetime: typeof parsetime;
+    bytesToBase64: typeof bytesToBase64;
+    base64ToBytes: typeof base64ToBytes;
   };
 };
 
@@ -32,4 +38,6 @@ _export["@hdml/hash"] = {
   hashify,
   hashtime,
   parsetime,
+  bytesToBase64,
+  base64ToBytes,
 };
